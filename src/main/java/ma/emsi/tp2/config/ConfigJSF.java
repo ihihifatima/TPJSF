e295cf23de09730b73936a4b83aa46d1b4757b6e
ihihifatima <ihihifatima@outlook.com>
2021-11-17 22:47:20 +0100

    Exercice 1

---
+++ src/main/java/ma/emsi/tp2/config/ConfigJSF.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ma.emsi.tp2.config;
+
+/**
+ *
+ * @author ihihi
+ */
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
